a6ee50a30ed84520466a2c56a0bcb445a1d3f757
Sam Avento <eazomc@gmail.com>
2023-12-25 11:40:25 +0700

    Update ChatScreen.js

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,35 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native'
 import React, { useState, useCallback, useEffect } from 'react'
-import { GiftedChat, Bubble, Send } from 'react-native-gifted-chat'
+import { GiftedChat } from 'react-native-gifted-chat'
 import { authentication } from '../firebaseConfig';
 import { addDoc, collection, serverTimestamp, doc, onSnapshot, query, orderBy } from 'firebase/firestore';
 import { db } from '../firebaseConfig';
-
-const renderBubble = (props) => {
-    return (
-        <Bubble
-            {...props}
-            wrapperStyle={{
-                right: {
-                    backgroundColor: '#3ECE00',
-                },
-                left: {
-                    backgroundColor: '#E5E5EA',
-                },
-            }}
-        />
-    );
-};
-
-const renderSend = (props) => {
-    return (
-        <Send {...props}>
-            <View style={styles.sendButton}>
-                <Text style={styles.sendButtonText}>Send</Text>
-            </View>
-        </Send>
-    );
-};
 
 export default function ChatScreen({ route }) {
     const uid = route.params.uid
@@ -81,43 +55,39 @@ export default function ChatScreen({ route }) {
     }, [])
 
     return (
-        <View style={styles.container}>
-            <GiftedChat
-                messages={messages}
-                onSend={text => onSend(text)}
-                user={{
-                    _id: currentUser,
-                }}
-                containerStyle={styles.chatContainer}
-                renderBubble={renderBubble}
-                renderSend={renderSend}
-            />
-        </View>
-    );
+        <GiftedChat
+            messages={messages}
+            onSend={text => onSend(text)}
+            user={{
+                _id: currentUser,
+            }}
+            renderSend={(props) => (
+                <TouchableOpacity
+                    {...props}
+                    style={styles.sendButton}
+                    onPress={() => {
+                        if (props.text && props.onSend) {
+                            props.onSend({ text: props.text.trim() }, true);
+                        }
+                    }}
+                >
+                    <Text style={styles.sendButtonText}>Send</Text>
+                </TouchableOpacity>
+            )}
+        />
+    )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#f0f0f0',
-    },
-    chatContainer: {
-        flex: 1,
-        marginBottom: 0,
-    },
     sendButton: {
-        justifyContent: 'center',
-        alignItems: 'center',
+        marginTop: 10,
         marginRight: 10,
-        height: '100%',
-        backgroundColor: '#007BFF',
-        borderRadius: 15,
-        paddingHorizontal: 15,
-        width: 100,
+        marginBottom: 10,
+        borderRadius: 20,
+        padding: 10,
+        backgroundColor: '#007AFF',
     },
     sendButtonText: {
-        color: 'white',
-        fontWeight: 'bold',
-        fontSize: 18,
-    },
-});
+        color: '#fff',
+    }
+})
