4c5e2f0bc250523d3e0da901978fa0643c6f572a
Sam Avento <eazomc@gmail.com>
2023-12-25 15:59:51 +0700

    Update login alert

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -12,14 +12,10 @@ const LoginScreen = ({ navigation }) => {
         const auth = getAuth();
         signInWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
-                // Signed in 
                 const user = userCredential.user;
-                // ...
             })
             .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                alert(errorMessage);
+                alert('Incorrect Email or Password');
             });
     }
 
