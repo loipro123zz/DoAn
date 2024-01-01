2b5770ab592db987bcd7d7f3ab936416123ef34a
Sam Avento <eazomc@gmail.com>
2024-01-01 21:30:29 +0700

    Update ChatScreen.js
    minor style changes

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -218,6 +218,7 @@ const styles = StyleSheet.create({
         borderTopWidth: 1,
         borderTopColor: '#E8E8E8',
         backgroundColor: 'white',
+        paddingBottom: 10,
     },
     textInput: {
         color: 'black',
