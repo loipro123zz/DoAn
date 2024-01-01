e3d98ffce0c1eafe3baa6529d1a7b5dbddf80871
Sam Avento <eazomc@gmail.com>
2024-01-01 19:23:51 +0700

    Added styling for Login and Register screen
    Files modified:
    - LoginScreen.js
    - RegisterScreen.js

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,15 +1,11 @@
-import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, Image } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { Input, Button } from 'react-native-elements';
 import { } from '../firebaseConfig'
 import { getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
-
-
 const LoginScreen = ({ navigation }) => {
-
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-
     const signIn = () => {
         const auth = getAuth();
         signInWithEmailAndPassword(auth, email, password)
@@ -22,6 +18,7 @@ const LoginScreen = ({ navigation }) => {
                 const errorCode = error.code;
                 const errorMessage = error.message;
                 alert(errorMessage);
+                alert('Incorrect Email or Password');
             });
     }
 
@@ -36,38 +33,47 @@ const LoginScreen = ({ navigation }) => {
                 // ...
             }
         });
+        navigation.setOptions({ headerShown: false });
         return unsubcribe;
     })
-
     return (
-        <View style={styles.container}>
-
-            <Input
-                placeholder='Enter your Email'
-                label='Email'
-                leftIcon={{ name: 'email', type: 'material' }}
-                value={email}
-                onChangeText={text => setEmail(text)}
+        <SafeAreaView style={styles.container}>
+            <Image
+                source={{ uri: 'https://pics.freeicons.io/uploads/icons/png/1165514541601363739-512.png' }}
+                style={styles.logo}
             />
-
-            <Input
-                placeholder='Enter your Password'
-                label='Password'
-                leftIcon={{ name: 'lock', type: 'material' }}
-                value={password}
-                onChangeText={text => setPassword(text)}
-                secureTextEntry
-            />
-
-            <Button title='Sign in' buttonStyle={styles.button} onPress={signIn} />
-            <Button title='Register' buttonStyle={styles.button}
-                onPress={() => navigation.navigate('Register')}
-            />
-
-        </View>
-    )
+            <Text style={styles.title}>Login to ChatApp!</Text>
+            <View style={styles.inputContainer}>
+                <Input
+                    placeholder='Enter your Email'
+                    label='Email'
+                    labelStyle={styles.inputLabel}
+                    leftIcon={{ name: 'email', type: 'material', marginRight: 10, marginLeft: 15, }}
+                    value={email}
+                    onChangeText={text => setEmail(text)}
+                    inputContainerStyle={styles.inputField}
+                />
+                <Input
+                    placeholder='Enter your Password'
+                    label='Password'
+                    labelStyle={styles.inputLabel}
+                    leftIcon={{ name: 'lock', type: 'material', marginRight: 10, marginLeft: 15, }}
+                    value={password}
+                    onChangeText={text => setPassword(text)}
+                    secureTextEntry
+                    inputContainerStyle={styles.inputField}
+                />
+            </View>
+            <Button title='Login' buttonStyle={styles.button} onPress={signIn} />
+            <Text style={styles.registerText}>
+                Don't have an account yet?
+                <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+                    <Text style={styles.registerLink}> Register now!</Text>
+                </TouchableOpacity>
+            </Text>
+        </SafeAreaView>
+    );
 }
-
 export default LoginScreen
 const styles = StyleSheet.create({
     button: {
@@ -76,8 +82,52 @@ const styles = StyleSheet.create({
     },
     container: {
         flex: 1,
-        padding: 10,
+        padding: 20,
         alignItems: 'center',
-    }
-})
-
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 36,
+        fontWeight: 'bold',
+        marginBottom: 30,
+    },
+    inputContainer: {
+        width: '100%',
+        marginBottom: 10,
+    },
+    inputLabel: {
+        fontSize: 20,
+        marginLeft: 10,
+        marginBottom: 5,
+    },
+    inputField: {
+        borderWidth: 1,
+        borderRadius: 50,
+        borderColor: '#000000',
+        marginBottom: 5,
+    },
+    button: {
+        width: 150,
+        marginTop: 10,
+        borderRadius: 25,
+    },
+    registerText: {
+        marginTop: 5,
+        fontSize: 16,
+        textAlign: 'center',
+    },
+    registerLink: {
+        color: 'blue',
+        fontSize: 16,
+        marginLeft: 5,
+        marginTop: 63,
+        textDecorationLine: 'underline',
+        marginBottom: -3,
+    },
+    logo: {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+        marginBottom: 20,
+    },
+});
--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, SafeAreaView, TouchableOpacity, Text } from 'react-native';
 import { Input, Button } from 'react-native-elements';
-import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { getFirestore, doc, setDoc } from 'firebase/firestore';
 import { authentication } from '../firebaseConfig';
 
@@ -13,82 +13,127 @@ const RegisterScreen = ({ navigation }) => {
 
     const registerUser = async () => {
         try {
-            const auth = getAuth();
-            const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
-            const user = userCredentials.user;
-            const userUID = user.uid;
+            const userCredentials = await createUserWithEmailAndPassword(authentication, email, password);
+            const userUID = userCredentials.user.uid;
             const docRef = doc(getFirestore(), 'users', userUID);
 
             await setDoc(docRef, {
                 avatarUrl: avatar ? avatar : 'https://thumbs.dreamstime.com/b/businessman-avatar-line-icon-vector-illustration-design-79327237.jpg',
                 username,
+                password,
                 userUID,
                 email
             });
 
-            // Cập nhật photoURL trong Firebase Authentication
-            await updateProfile(user, {
-                photoURL: avatar ? avatar : 'https://thumbs.dreamstime.com/b/businessman-avatar-line-icon-vector-illustration-design-79327237.jpg',
-            });
-
-            // Log để kiểm tra
-            console.log('Successful registration');
-            console.log('User photoURL:', user.photoURL);
+            alert('Successful registration');
         } catch (error) {
-            console.error('Error during registration:', error);
+            alert('Invalid Email or Password')
         }
     };
 
+    useEffect(() => {
+        navigation.setOptions({ headerShown: false });
+    }, []);
+
     return (
-        <View style={styles.container}>
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>Create an account today!</Text>
+
             <Input
                 placeholder='Enter your Name'
-                label='username'
-                leftIcon={{ name: 'people', type: 'material' }}
+                label='Username'
+                labelStyle={styles.inputLabel}
+                leftIcon={{ name: 'people', type: 'material', marginRight: 10, marginLeft: 15 }}
                 value={username}
                 onChangeText={text => setUsername(text)}
+                inputContainerStyle={styles.inputField}
             />
 
             <Input
                 placeholder='Enter your Email'
                 label='Email'
-                leftIcon={{ name: 'email', type: 'material' }}
+                labelStyle={styles.inputLabel}
+                leftIcon={{ name: 'email', type: 'material', marginRight: 10, marginLeft: 15 }}
                 value={email}
                 onChangeText={text => setEmail(text)}
+                inputContainerStyle={styles.inputField}
             />
 
             <Input
                 placeholder='Enter your Password'
                 label='Password'
-                leftIcon={{ name: 'lock', type: 'material' }}
+                labelStyle={styles.inputLabel}
+                leftIcon={{ name: 'lock', type: 'material', marginRight: 10, marginLeft: 15 }}
                 value={password}
                 onChangeText={text => setPassword(text)}
                 secureTextEntry
+                inputContainerStyle={styles.inputField}
             />
 
             <Input
                 placeholder='Enter your Image URL'
                 label='Avatar'
-                leftIcon={{ name: 'link', type: 'material' }}
+                labelStyle={styles.inputLabel}
+                leftIcon={{ name: 'link', type: 'material', marginRight: 10, marginLeft: 15 }}
                 value={avatar}
                 onChangeText={text => setAvatar(text)}
+                inputContainerStyle={styles.inputField}
             />
 
             <Button title='Register' buttonStyle={styles.button} onPress={registerUser} />
-        </View>
+
+            <Text style={styles.loginText}>
+                Already have an account?
+                <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+                    <Text style={styles.loginLink}> Login now!</Text>
+                </TouchableOpacity>
+            </Text>
+        </SafeAreaView>
     );
 };
+
+export default RegisterScreen;
 
 const styles = StyleSheet.create({
     button: {
         width: 200,
         marginTop: 10,
+        borderRadius: 25,
     },
     container: {
         flex: 1,
-        padding: 10,
+        padding: 20,
         alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 36,
+        fontWeight: 'bold',
+        marginBottom: 30,
+        textAlign: 'center',
+    },
+    inputLabel: {
+        fontSize: 20,
+        marginLeft: 10,
+        marginBottom: 5,
+    },
+    inputField: {
+        borderWidth: 1,
+        borderRadius: 50,
+        borderColor: '#000000',
+        marginBottom: 10,
+    },
+    loginText: {
+        marginTop: 5,
+        fontSize: 16,
+        textAlign: 'center',
+    },
+    loginLink: {
+        color: 'blue',
+        fontSize: 16,
+        marginLeft: 5,
+        marginTop: 63,
+        textDecorationLine: 'underline',
+        marginBottom: -3,
     },
 });
-
-export default RegisterScreen;
